0cae1a6edfb3707ddf3784f6b58f6387057426df
Ezequiel <emancilla01@gmail.com>
2025-05-27 13:36:53 -0500

    changed insact compras

--- compras/ins_act.php
+++ compras/ins_act.php
@@ -21,7 +21,7 @@
   $sql = "INSERT INTO compras (proveedor_id, total, fecha) 
                 VALUES ('$proveedor_id', '$total', '$fecha')";
   $compras->insertar($sql);
-  include "../compras/tabla.php";
+  include "../compras/index.php";
   
   $compras->desconectar();
 ?> 
